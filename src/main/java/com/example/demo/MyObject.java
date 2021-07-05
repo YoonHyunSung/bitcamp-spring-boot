c457d650156c77ba1ea8d47c85d699825ce42d40
YoonHyunSung <gustjd6495@gmail.com>
2021-07-05 09:40:51 +0900

    first commit

---
+++ src/main/java/com/example/demo/MyObject.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.Scanner;
+
+public class MyObject {
+    public static void main(String[] args) {
+        System.out.println("숫자 1");
+        Scanner scanner = new Scanner(System.in);//new 를 통하여 객체 생성
+        int num1 = scanner.nextInt();// 값을 저장 next() 값을 읽음. /* int num1 = scanner.nextInt(); float num1 = scanner.nextfloat(); 타입별로 변경가능 */
+        System.out.println("숫자 2");
+        int num2 = scanner.nextInt();  /*Scanner scanner2 = new Scanner(System.in);재사용성*/
+        int num3 = num1 + num2;
+        System.out.printf("%d + %d = %d", num1, num2, num3);//%변수지정 %d10=진수의 변수
+        }
+}
+;
