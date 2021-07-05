d588b36c3ddcf57b7d6bf3ecb5f3123c3cc42196
YoonHyunSung <gustjd6495@gmail.com>
2021-07-05 18:25:26 +0900

    first commit

--- src/main/java/com/example/demo/Tv.java
+++ src/main/java/com/example/demo/Tv.java
@@ -1,45 +1,55 @@
 package com.example.demo;
-class TvControl{
-    public static void main(String[] args) {
-        Tv a1 = new Tv("lg");
+class TvControl {
+    public static void main(String[] args){
+        Tv lgTv = new Tv("LG");
         Tv sTv = new Tv("samsung");
-        Tv hTv = new Tv("HP",2048);
-        hTv.current_channel =11;
+        Tv hTv = new Tv("HP", 2048);
+        System.out.println(hTv.current_channel);
+        hTv.setCurrent_channel(11);
+        System.out.println(hTv.getCurrent_channel());
         System.out.println(hTv.resolution);
-        // Tv on
-        // 볼륨 키우기
-        // 채널 바꾸기
+// Tv 켜기
+// 볼륨 키우기
+// 채널 바꾸기
     }
 }
+public class Tv {
+    // 속성
+    String model_name;
+    int[] channel;
+    int current_channel;
+    int resolution;
+    boolean power;
 
+    Tv(String tv_name) {
+        model_name = tv_name;
+        resolution = 1024;
+        power = false;
+        current_channel = 0;
+        channel = new int[100];
+        for (int i = 0; i < channel.length; i++) {
+            channel[i] = i + 1;
+        }
+    }
 
+    Tv(String tv_name, int resolution) {
+        model_name = tv_name;
+        this.resolution = resolution;
+        power = false;
+        current_channel = 0;
+        channel = new int[100];
+        for (int i = 0; i < channel.length; i++) {
+            channel[i] = i + 1;
+        }
+    }
 
- class Tv {
-     //속성
-     String model_Name;
-     int[] channel;
-     int current_channel;
-     int resolution;
-     boolean power;
-     String tv_name;
+    // Getter Setter
+    public int getCurrent_channel() {
+        return current_channel;
+    }
 
-     Tv(String tv_name) {
-         model_Name = "lgtv";
-         resolution = 1024;
-         power = false;
-         current_channel = 0;
-         channel = new int[100];
-         for (int i = 0; i < channel.length; i++) {
-             channel[i] = i + 1;
-
-         }
-     }//빈값일때 디폴트 생성자가 잡힌다 안에 코드입력시 디폴트가 풀림.
-     Tv(String tv_name, int resolution){
-
-     }
-     //기능
-
-     class TvAttribute {
-
-     }
- }
+    public void setCurrent_channel(int current_channel) {
+        this.current_channel = current_channel;
+    }
+}
+// 기능
